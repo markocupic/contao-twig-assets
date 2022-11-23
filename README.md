23bd6cc97f953f782150df80965b37198712ab33
Marko Cupic <m.cupic@gmx.ch>
2022-11-23 14:58:03 +0100

    Add a second optional parameter to each function, to make it possible to set a fixed position for the asset inside the $GLOBALS resource stack.

--- README.md
+++ README.md
@@ -1,30 +1,54 @@
 ![Alt text](docs/logo.png?raw=true "logo")
 
 
-# Welcome to Contao Twig Assets
+# Contao Twig Assets
 Adding assets via the global array `$GLOBALS` is no more possible when using TWIG templates.
-This Bundle for the Contao CMS provides 5 TWIG functions to load assets from inside your TWIG template.
+This Bundle for the Contao CMS provides **5 TWIG functions** to load assets from inside your **TWIG template**.
 
+
+## Usage
+
+Each of these five functions take two parameters e.g. addJavascriptResource($resource_path, $position)
+The first parameter '$resource_path' is mandatory, while the second parameter '$position' is optional.
+
+
+**Default usage:**
 ```
-{# Inside your TWIG template: #}
+{# Inside your twig template: #}
 
-
-{# Sames as $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/contaofoobarplugin/js/my.js|static'; #}
 {% do addJavascriptResource('bundles/contaofoobarplugin/js/my.js|static') %}
+{# Same as $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/contaofoobarplugin/js/my.js|static'; #}
+```
+
+**Set a javascript resource at a fixed position or replacing it:**
+```
+{# Inside your twig template: #}
+
+{% do addJavascriptResource('bundles/contaofoobarplugin/js/my.js|static', 2) %}
+{# Same as $GLOBALS['TL_JAVASCRIPT'][2] = 'bundles/contaofoobarplugin/js/my.js|static'; #}
+```
 
 
-{# Sames as $GLOBALS['TL_CSS'][] = 'bundles/contaofoobarplugin/css/style.css|static'; #}
+## Overview
+```
+{# Inside your twig template: #}
+
+{% do addJavascriptResource('bundles/contaofoobarplugin/js/my.js|static') %}
+{# Same as $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/contaofoobarplugin/js/my.js|static'; #}
+
+
 {% do addCssResource('bundles/contaofoobarplugin/css/style.css|static') %}
+{# Same as $GLOBALS['TL_CSS'][] = 'bundles/contaofoobarplugin/css/style.css|static'; #}
 
 
-{# Sames as $GLOBALS['TL_HEAD'][] = '<script src="bundles/contaofoobarplugin/script.js"></script>'; #}
 {% do addHtmlToHead('<script src="bundles/contaofoobarplugin/script.js"></script>') %}
+{# Same as $GLOBALS['TL_HEAD'][] = '<script src="bundles/contaofoobarplugin/script.js"></script>'; #}
 
 
-{# Sames as $GLOBALS['TL_BODY'][] = '<script src="bundles/contaofoobarplugin/script.js"></script>'; #}
 {% do addHtmlToBody('<script src="bundles/contaofoobarplugin/scripts.js"></script>') %}
+{# Same as $GLOBALS['TL_BODY'][] = '<script src="bundles/contaofoobarplugin/script.js"></script>'; #}
 
 
-{# Sames as $GLOBALS['TL_MOOTOOLS'][] = '<script src="bundles/contaofoobarplugin/moo_script.js"></script>'; #}
 {% do addMootoolsResource('<script src="bundles/myextension/moo_scripts.js"></script>') %}
+{# Same as $GLOBALS['TL_MOOTOOLS'][] = '<script src="bundles/contaofoobarplugin/moo_script.js"></script>'; #}
 ```
--- src/Twig/Extension/TwigAssetManager.php
+++ src/Twig/Extension/TwigAssetManager.php
@@ -40,81 +40,97 @@ class TwigAssetManager extends AbstractExtension
      * Append CSS Resource from inside your Twig template.
      *
      * Inside your Twig template:
-     * {% do addCssResource('bundles/foobar/css/my.css|static %}.
+     * {% do addCssResource('bundles/foobar/css/my.css|static') %}.
      *
      * @see: https://docs.contao.org/dev/framework/asset-management.
      */
-    public function addCssResource(string $res): void
+    public function addCssResource(string $res, int|string|null $pos = null): void
     {
-        $GLOBALS[DocumentLocation::TL_CSS->value][] = $res;
+        $this->addResource(DocumentLocation::TL_CSS->value, $res, $pos);
     }
 
     /**
      * Append Javascript Resource from inside your Twig template.
      *
      * Inside your Twig template:
-     * {% do addJavascriptResource('bundles/foobar/js/my.js|static %}.
+     * {% do addJavascriptResource('bundles/foobar/js/my.js|static') %}.
      *
      * @see: https://docs.contao.org/dev/framework/asset-management.
      */
-    public function addJavascriptResource(string $res): void
+    public function addJavascriptResource(string $res, int|string|null $pos = null): void
     {
-        $GLOBALS[DocumentLocation::TL_JAVASCRIPT->value][] = $res;
+        $this->addResource(DocumentLocation::TL_JAVASCRIPT->value, $res, $pos);
     }
 
     /**
      * Include HTML before </body>.
      *
      * Inside your Twig template:
-     * {% do addMootoolsResource('<script src="bundles/myextension/scripts.js"></script> %}.
+     * {% do addMootoolsResource('<script src="bundles/myextension/scripts.js"></script>') %}.
      *
      * @see: https://docs.contao.org/dev/framework/asset-management.
      */
-    public function addMootoolsResource(string $res): void
+    public function addMootoolsResource(string $res, int|string|null $pos = null): void
     {
-        $GLOBALS[DocumentLocation::TL_MOOTOOLS->value][] = $res;
+        $this->addResource(DocumentLocation::TL_MOOTOOLS->value, $res, $pos);
     }
 
     /**
      * Include HTML before </body>.
      *
      * Inside your Twig template:
-     * {% do addHtmlToBody('<script src="bundles/myextension/scripts.js"></script> %}.
+     * {% do addHtmlToBody('<script src="bundles/myextension/scripts.js"></script>') %}.
      *
      * @see: https://docs.contao.org/dev/framework/asset-management.
      */
-    public function addHtmlToBody(string $res): void
+    public function addHtmlToBody(string $res, int|string|null $pos = null): void
     {
-        $GLOBALS[DocumentLocation::TL_BODY->value][] = $res;
+        $this->addResource(DocumentLocation::TL_BODY->value, $res, $pos);
     }
 
     /**
      * Include HTML in the <head> of your document.
      *
      * Inside your Twig template:
-     * {% do addHtmlToHead('<script src="bundles/myextension/scripts.js"></script> %}.
+     * {% do addHtmlToHead('<script src="bundles/myextension/scripts.js"></script>') %}.
      *
      * @see: https://docs.contao.org/dev/framework/asset-management.
      */
-    public function addHtmlToHead(string $res): void
+    public function addHtmlToHead(string $res, int|string|null $pos = null): void
     {
-        $GLOBALS[DocumentLocation::TL_HEAD->value][] = $res;
+        $this->addResource(DocumentLocation::TL_HEAD->value, $res, $pos);
+    }
+
+    public function addResource(string $location, string $res, int|string|null $pos = null): void
+    {
+        if (!\in_array($location, $this->getGlobalsKeys(), true)) {
+            throw new \Exception("'%s' is not a valid asset location.");
+        }
+
+        if ($pos) {
+            $GLOBALS[$location][$pos] = $res;
+        } else {
+            $GLOBALS[$location][] = $res;
+        }
     }
 
     private function createGlobalsIfNotSet(): void
     {
-        $keys = [
+        foreach ($this->getGlobalsKeys() as $key) {
+            if (!isset($GLOBALS[$key])) {
+                $GLOBALS[$key] = [];
+            }
+        }
+    }
+
+    private function getGlobalsKeys(): array
+    {
+        return [
             DocumentLocation::TL_CSS->value,
             DocumentLocation::TL_JAVASCRIPT->value,
             DocumentLocation::TL_HEAD->value,
             DocumentLocation::TL_BODY->value,
             DocumentLocation::TL_MOOTOOLS->value,
         ];
-
-        foreach ($keys as $key) {
-            if (!isset($GLOBALS[$key])) {
-                $GLOBALS[$key] = [];
-            }
-        }
     }
 }
